93b26937f7e85ddd1f5a73fee62effa6001c9bc9
Yury <dethdiez7@gmail.com>
2018-05-22 07:33:35 +0300

    just

--- api/test_db.py
+++ api/test_db.py
@@ -8,6 +8,7 @@ client = MongoClient('mongodb://0.0.0.0:27017/')
 db = client['diplom_mongo_1']
 
 #post = {"author": "Yury", "text": "test post", "tags": ["mongoDB","python"]}
+'''
 jsonFile = open("example.json", "r")
 data = json_util.loads(jsonFile.read())
 posts = db.posts
@@ -16,6 +17,7 @@ cursor = posts.find({})
 for document in cursor: 
 	print(document)
 posts.delete_many({})
+'''
 
 def CleanDB():
 	client = MongoClient('mongodb://0.0.0.0:27017/')
