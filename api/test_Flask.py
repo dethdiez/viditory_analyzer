835a7f8784303a54bf4e0e1af3b71d12539ac390
Yury <dethdiez7@gmail.com>
2018-06-05 06:16:23 +0300

    try test POST

--- api/test_Flask.py
+++ api/test_Flask.py
@@ -21,10 +21,8 @@ def analyze():
 
 @app.route('/new', methods=['POST'])
 def new():
-	item_doc = {
-	'task': request.args.get("task"),
-	'description': request.args.get("description")
-	}
+	task = request.args.get("task"),
+	description = request.args.get("description")
 	#db.tododb.insert_one(item_doc)
 	return (item_doc)
 
